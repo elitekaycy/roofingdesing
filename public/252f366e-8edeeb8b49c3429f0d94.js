a5fda11e576884d36e3e070f57e6968f03846d06
elitekaycy <dicksonanyaele1234@gmail.com>
2021-08-24 18:57:45 +0000

    roofingdesign

---
+++ public/252f366e-8edeeb8b49c3429f0d94.js
@@ -0,0 +1,2 @@
+(self.webpackChunkroofingdesign=self.webpackChunkroofingdesign||[]).push([[228],{2775:function(n,t,c){"use strict";c.d(t,{ixJ:function(){return e}});var i=c(1046);function e(n){return(0,i.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"}}]})(n)}}}]);
+//# sourceMappingURL=252f366e-8edeeb8b49c3429f0d94.js.map
